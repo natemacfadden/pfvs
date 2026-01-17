a0e799c47ddfa9da6e8720cf9c024db8a9b4ea1b
Nate MacFadden <natemacfadden@gmail.com>
2026-01-17 15:45:59 -0500

    better working gurobi method

--- smarter_conipfv.py
+++ smarter_conipfv.py
@@ -2,13 +2,18 @@ import gurobipy as gp
 import math
 import numpy as np
 
-def coniMan(Binter, Z, Qmin, Qmax, M0min, M0max=None, K0min=None, K0max=None,
+def coniMan(Binter, Z, Qmin, Qmax, M0min,
+            M0max=None,
+            K0min=None, K0max=None,
+            Kbounds = 100,
+            Mbounds = 100,
             Nsol: int = 100,
             timelimit: float=None,
             verbosity: int = 0):
-
+    
     # helper variables
     h11 = Binter.shape[0]
+    assert Binter.shape[1] == h11-1
     e0  = np.array([1]+[0]*(h11-1))
     
     # define the model
@@ -18,58 +23,75 @@ def coniMan(Binter, Z, Qmin, Qmax, M0min, M0max=None, K0min=None, K0max=None,
         model.setParam('TimeLimit', timelimit)  # 60 seconds
 
     # variables
-    c_alpha = model.addMVar(
-        (Binter.shape[1]+1,),
-        lb=-float('inf'), ub=float('inf'),
-        vtype=gp.GRB.INTEGER)
+    # =========
+    # physically relevant variables
+    # -----------------------------
+    # (define K := (Z@Binter@c + alpha*e0) / K_scale)
+    # (equiv: K*K_scale - alpha*e0 = Z@Binter@c)
+    M       = [model.addVar(lb=-Mbounds, ub=Mbounds, vtype=gp.GRB.INTEGER) for i in range(h11)]
+    K       = [model.addVar(lb=-Kbounds, ub=Kbounds, vtype=gp.GRB.INTEGER) for i in range(h11)]
+    K_scale = model.addVar(lb=1, ub=float('inf'), vtype=gp.GRB.INTEGER)
 
-    projc = np.hstack([
-        np.identity(Binter.shape[1],dtype=int),
-        np.zeros((Binter.shape[1],1),dtype=int)
-    ])
+    # internal variables
+    # ------------------
+    c        = [model.addVar(lb=-float('inf'), ub=float('inf'), vtype=gp.GRB.INTEGER) for i in range(h11-1)]
+    alpha    = model.addVar(lb=-float('inf'), ub=float('inf'), vtype=gp.GRB.INTEGER)
     
     # constraints
-    # -----------
-    # tadpole constraints
-    topleft  = -Binter.T@Z@Binter
-    topright = (-Binter.T@e0/2).reshape(-1,1)
-    botleft  = (-e0.T@Binter/2).reshape(1,-1)
-    tmp = np.block([[topleft, topright],
-                    [botleft, 0]])
-    model.addMQConstr(Q=tmp, c=None, sense='>', rhs=Qmin-1, xQ_L=c_alpha, xQ_R=c_alpha)
-    model.addMQConstr(Q=tmp, c=None, sense='<', rhs=Qmax+1, xQ_L=c_alpha, xQ_R=c_alpha)
+    # ===========
+    # M constraints
+    # -------------
+    # enforce M == Binter@c
+    for i in range(h11):
+        model.addConstr(
+            M[i] == gp.quicksum([Binter[i,j] * c[j] for j in range(h11-1)])
+        )
 
-    # coni-M constraints
-    model.addConstr(e0.T @ Binter @ projc@c_alpha >= M0min)
-    if M0max is not None:
-        model.addConstr(e0.T @ Binter @ projc@c_alpha <= M0max)
-
-    # coni-K constraints
-    K0_extractor = np.hstack([e0.T @ Z@Binter, [1]])
-    if K0min is not None:
-        model.addConstr(K0_extractor @ c_alpha >= K0min) # e0.T @ Z@Binter @ c + alpha >= K0min
-    if K0max is not None:
-        model.addConstr(K0_extractor @ c_alpha <= K0max) # e0.T @ Z@Binter @ c + alpha <= K0max
-
-    # Kprime constraints
-    #-self.K[0] + (self.M@self._cydata.kappa_cob@self.p)[0]
-    #-K0_extractor + 
+    # M0 constraints
+    if M0min is not None: model.addConstr(M[0] >= M0min)
+    if M0max is not None: model.addConstr(M[0] <= M0max)
     
-    #model.addMQConstr(
-    #    Q = -Binter.T @ Z @ Binter,
-    #    c = -
+    # K constraints
+    # -------------
+    # enforce K == Z@Binter@c
+    for i in range(h11):
+        if i == 0:
+            alphaterm = alpha
+        else:
+            alphaterm = 0
+    
+        model.addQConstr(
+            K[i]*K_scale - alphaterm,
+            gp.GRB.EQUAL,
+            gp.quicksum([(Z@Binter)[i,j] * c[j] for j in range(h11-1)])
+        )
+
+    # K0 constraints
+    if K0min is not None: model.addConstr(K[0]+alpha >= K0min)
+    if K0max is not None: model.addConstr(K[0]+alpha <= K0max)
+
+    # tadpole constraints
+    # -------------------
+    model.addQConstr(K[0]*M[0] + gp.quicksum([K[i]*M[i] for i in range(1,h11)]) <= -Qmin)
+    model.addQConstr(K[0]*M[0] + gp.quicksum([K[i]*M[i] for i in range(1,h11)]) >= -Qmax)
 
     # optimize for all solutions
+    # ==========================
     model.setParam('PoolSearchMode', 2)
     model.setParam('PoolSolutions', Nsol)
 
     model.optimize()
 
     # read the solutions
-    sols = []
+    Ks = []
+    Ms = []
     for i in range(model.SolCount):
         model.setParam('SolutionNumber', i)
 
-        sols.append(np.rint(c_alpha.xn).astype(int))
+        Kval = [K[i].xn for i in range(h11)]
+        Mval = [M[i].xn for i in range(h11)]
 
-    return np.vstack(sols)
+        Ks.append(np.rint(Kval).astype(int))
+        Ms.append(np.rint(Mval).astype(int))
+
+    return np.vstack(Ks), np.vstack(Ms)
