f5988d4da2fa87df9fd41b5530b7a6097015f907
Nate MacFadden <natemacfadden@gmail.com>
2026-02-02 11:31:32 -0500

    simple c code for box enumeration
    using this as a warm-up for coni-Kernel

---
+++ src/enumerate_box/enumerate_box.c
@@ -0,0 +1,64 @@
+#include "enumerate_box.h"
+#include <stdlib.h>
+
+int enumerate_box_c(
+    const int32_t *bounds,
+    int dim,
+    int max_N_out,
+    int32_t *out,
+    int *N_out)
+{
+    // allocate memory for stack variables
+    int32_t *vec = calloc(dim, sizeof(int32_t));
+    int32_t *stack_pos = calloc(dim, sizeof(int32_t));
+    int32_t *stack_len = calloc(dim, sizeof(int32_t));
+
+    if (!vec || !stack_pos || !stack_len)
+        return -1;
+
+    // set # of components per dimension
+    for (int i = 0; i < dim; ++i)
+        stack_len[i] = 2 * bounds[i] + 1;
+
+    // iterate over the stack
+    int stack_i = 0;
+    int op = 0;
+
+    while (stack_i >= 0) {
+        // check if we exhausted values for this component
+        if (stack_pos[stack_i] == stack_len[stack_i]) {
+            stack_i--;
+            if (stack_i >= 0)
+                // increment position of prior element
+                stack_pos[stack_i]++;
+            continue;
+        }
+
+        // set vec[stack_i]
+        vec[stack_i] = -bounds[stack_i] + stack_pos[stack_i];
+
+        // check if done
+        if (stack_i == dim - 1) {
+            if (op >= max_N_out)
+                return -2;
+
+            for (int j = 0; j < dim; ++j)
+                out[op * dim + j] = vec[j];
+
+            op++;
+            stack_pos[stack_i]++;
+        } else {
+            // not done -move on to next component
+            stack_i++;
+            stack_pos[stack_i] = 0;
+        }
+    }
+
+    *N_out = op;
+
+    // free variabls
+    free(vec);
+    free(stack_pos);
+    free(stack_len);
+    return 0;
+}
---
+++ src/enumerate_box/enumerate_box.h
@@ -0,0 +1,14 @@
+#ifndef ENUMERATE_BOX_H
+#define ENUMERATE_BOX_H
+
+#include <stdint.h>
+
+int enumerate_box_c(
+    const int32_t *bounds,
+    int dim,
+    int max_N_out,
+    int32_t *out,   // shape: [max_N_out, dim]
+    int *N_out      // number of rows written
+);
+
+#endif
