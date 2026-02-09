c6cb8477ad2d4559d950ba0d5def3c3413875622
Nate MacFadden <natemacfadden@gmail.com>
2026-02-09 18:37:14 -0500

    better hide the kernels a bit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # setup.py
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import os
 
-package_dir = "coni_kernel"
+package_dir = "pfvs/c_kernels"
 c_src_dir = os.path.join(package_dir, "c_code")
 
 extensions = [
     Extension(
-        name=f"{package_dir}.coni_kernel",
+        name=f"pfvs.c_kernels.coni_kernel",
         sources=[os.path.join(package_dir, "coni_kernel.pyx"),
                  os.path.join(c_src_dir, "coni_kernel.c")],
         include_dirs=[c_src_dir],
@@ -19,9 +19,10 @@ extensions = [
 
 setup(
     name="coni_kernel",
-    packages=[package_dir],
+    packages=find_packages(),
     ext_modules=cythonize(
         extensions,
         compiler_directives={"language_level": "3"},
     ),
+    zip_safe=False,
 )
