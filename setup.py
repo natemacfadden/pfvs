df59683cb15135ab315f3c4d2ce9757731b4c32f
Nate MacFadden <natemacfadden@gmail.com>
2026-02-12 21:57:20 -0500

    make this pip installable

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, Extension
+from setuptools.command.develop import develop
+from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
 import os
 
@@ -21,24 +23,41 @@ extensions = []
 package_path = "pfvs/c_kernels"
 
 for k in kernels:
-    ext = Extension(
-        f"pfvs.c_kernels.{k['name']}",
-        sources=[
-            os.path.join(package_path, k["pyx"]),
-        ],
-        include_dirs=[
-            os.path.join(package_path, k["include"]),
-        ],
-        define_macros=[
-            (k["impl"], None),
-        ],
-        language="c",
-        extra_compile_args=["-O3"],
+    extensions.append(
+        Extension(
+            f"pfvs.c_kernels.{k['name']}",
+            sources=[os.path.join(package_path, k["pyx"])],
+            include_dirs=[os.path.join(package_path, k["include"])],
+            define_macros=[(k["impl"], None)],
+            language="c",
+            extra_compile_args=["-O3"],
+        )
     )
-    extensions.append(ext)
+
+
+class build_ext(_build_ext):
+    """Custom build_ext to ensure extensions are built in-place for editable installs."""
+    def run(self):
+        self.inplace = 1  # Force in-place build
+        super().run()
+
+
+class DevelopCommand(develop):
+    """Custom develop command to build extensions in-place."""
+    def run(self):
+        # Build extensions in-place before running develop
+        self.reinitialize_command('build_ext', inplace=1)
+        self.run_command('build_ext')
+        super().run()
+
 
 setup(
-    name="c_kernels",
-    packages=["pfvs", "pfvs.c_kernels"],
-    ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={"language_level": "3"},
+    ),
+    cmdclass={
+        'build_ext': build_ext,
+        'develop': DevelopCommand,
+    },
 )
